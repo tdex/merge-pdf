e867624bb61302656346e9d414888c32068da5f6
Tarcísio Deschamps Silva <tardchamps94@gmail.com>
2020-07-27 09:41:22 -0300

    ajuste para verificar tamanho do arquivo e não pela quantidade.

--- src/main/java/br/com/nagem/docbrasil/PdfMerge.java
+++ src/main/java/br/com/nagem/docbrasil/PdfMerge.java
@@ -9,43 +9,50 @@ import org.apache.pdfbox.multipdf.PDFMergerUtility;
 
 public class PdfMerge {
 
+	private static double getFileSizeMegaBytes(File file) {
+		return (double) file.length() / (1024 * 1024);
+	}
+
 	public static void processaPdf() throws IOException {
 		PDFMergerUtility PDFmerger = new PDFMergerUtility();
 
 		String path = new File(".").getCanonicalPath();
 		File folder = new File(path);
 		String outputFolder = path + "/saida/";
-		
+
 		new File(outputFolder).mkdir();
-		
+
 //		String outputFolder = "/home/tarcisio/Área de Trabalho/teste/";
 //		File folder = new File("/home/tarcisio/Área de Trabalho/teste/");
-		
+
 		File[] listOfFiles = folder.listFiles();
 
-		int cont_arquivo = 0;
 		int cont_nome_arquivo = 1;
+		double tamanhoPDF = 0;
 
-
-		for (File file : listOfFiles) {
-			cont_arquivo++;
+		for (int i = 0; i < listOfFiles.length; i++) {
 			String nomeArquivoAtual = outputFolder + "parte_" + cont_nome_arquivo + ".pdf";
 
+			File file = listOfFiles[i];
+
 			if (file.isFile() && FilenameUtils.getExtension(file.getName().toLowerCase()).contains("pdf")) {
-				if (cont_arquivo <= 90) {
+				tamanhoPDF = tamanhoPDF + getFileSizeMegaBytes(file);
+
+				if (tamanhoPDF <= 14) {
 					PDFmerger.addSource(file);
 				} else {
 					System.out.println(nomeArquivoAtual);
 					PDFmerger.setDestinationFileName(nomeArquivoAtual);
 					PDFmerger.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly());
-					
+
 					PDFmerger = new PDFMergerUtility();
-					cont_arquivo = 0;
+					PDFmerger.addSource(file);
 					cont_nome_arquivo++;
+					tamanhoPDF = getFileSizeMegaBytes(file);
 				}
-			} 
+			}
 		}
-		
+
 		PDFmerger.setDestinationFileName(outputFolder + "parte_" + cont_nome_arquivo + ".pdf");
 		PDFmerger.mergeDocuments(MemoryUsageSetting.setupMainMemoryOnly());
 
